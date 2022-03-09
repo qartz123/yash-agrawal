0947ebe271bb51dd3140278c84c72d12cbfe8a86
qartz123 <yashagrawalsy@gmail.com>
2022-03-10 01:14:26 +0530

    Changes as per Ques 2 requirement

--- add.js
+++ add.js
@@ -4,12 +4,10 @@ exports.addString = function (inputString) {
         return 0;
     } else {
         var inputArray = inputString.split(',');
-        if(inputArray.length == 1) {
-            return inputArray[0];
-        } else if(inputArray.length == 2) {
-            return parseInt(inputArray[0]) + parseInt(inputArray[1]);
-        } else {
-            return "Not a valid String.";
-        }
+            var result = 0;
+            inputArray.forEach(element => {
+                result = result + parseInt(element);
+            });
+            return result;
     }
 }
--- input.js
+++ input.js
@@ -7,5 +7,5 @@ var oneNumberString = "1";
 console.log("Addition : "+ calculator.addString(oneNumberString));
 var twoNumberString = "1,3";
 console.log("Addition : "+ calculator.addString(twoNumberString));
-var invalidString = "1,3,4,5,6.6,5.4,3.";
+var invalidString = "1,3,4,5,6,5,3";
 console.log("Addition : "+ calculator.addString(invalidString));
